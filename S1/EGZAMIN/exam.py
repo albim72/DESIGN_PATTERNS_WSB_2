bf8c36c4f25202d95e74ae11daf72169294e47b5
albim72 <marcin.albiniak72@gmail.com>
2023-02-19 16:24:18 +0100

    Create exam.py

---
+++ S1/EGZAMIN/exam.py
@@ -0,0 +1,27 @@
+class Exam:
+    def __init__(self):
+        self._writing_grade = 0
+        self._math_grade = 0
+
+    @staticmethod
+    def _check_grade(value):
+        if not(0<=value<=100):
+            raise ValueError('ocena musi być wartością z zakresu 0-100')
+
+    @property
+    def writing_grade(self):
+        return self._writing_grade
+
+    @writing_grade.setter
+    def writing_grade(self,value):
+        self._check_grade(value)
+        self._writing_grade = value
+
+    @property
+    def math_grade(self):
+        return self._math_grade
+
+    @math_grade.setter
+    def math_grade(self, value):
+        self._check_grade(value)
+        self._math_grade = value
